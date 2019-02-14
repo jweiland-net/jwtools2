f3dfc9db98a4acc2435d835d729938450760b1b9
Stefan Froemken <froemken@gmail.com>
2019-02-14 15:25:39 +0100

    Add Hook to pre-convert URL for realurl

---
+++ Classes/RealUrl/ConvertTableAliasToId.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace JWeiland\Jwtools2\RealUrl;
+
+/*
+ * This file is part of the jwtools2 project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+use DmitryDulepov\Realurl\Decoder\UrlDecoder;
+
+/**
+ * Convert RealUrl alias of type lookUpTable to ID
+ */
+class ConvertTableAliasToId
+{
+    /**
+     * @param $tmp
+     */
+    public function convert(array $parameters, UrlDecoder $parentObject)
+    {
+        $bla = $tmp;
+    }
+}
