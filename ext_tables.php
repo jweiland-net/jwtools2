88eefa28b4544726414c015d41c8319d11ffdea1
Stefan Froemken <froemken@gmail.com>
2019-05-10 12:19:49 +0200

    Remove TYPO3_MODE check in ext_tables.php

--- ext_tables.php
+++ ext_tables.php
@@ -3,21 +3,19 @@ if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
 
-if (TYPO3_MODE === 'BE') {
-    /** Register JW Tools module */
-    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
-        'JWeiland.jwtools2',
-        'tools',
-        'tools',
-        '',
-        [
-            'Tools' => 'overview',
-            'Solr' => 'list, show, createIndexQueueForAllSites, showClearFullIndexForm, showClearIndexForm, clearIndex'
-        ],
-        [
-            'access' => 'user,group',
-            'icon' => 'EXT:jwtools2/Resources/Public/Icons/module_tools.svg',
-            'labels' => 'LLL:EXT:jwtools2/Resources/Private/Language/locallang_module_tools.xlf',
-        ]
-    );
-}
+/** Register JW Tools module */
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
+    'JWeiland.jwtools2',
+    'tools',
+    'tools',
+    '',
+    [
+        'Tools' => 'overview',
+        'Solr' => 'list, show, createIndexQueueForAllSites, showClearFullIndexForm, showClearIndexForm, clearIndex'
+    ],
+    [
+        'access' => 'user,group',
+        'icon' => 'EXT:jwtools2/Resources/Public/Icons/module_tools.svg',
+        'labels' => 'LLL:EXT:jwtools2/Resources/Private/Language/locallang_module_tools.xlf',
+    ]
+);
