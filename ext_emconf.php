f0ffc5223f740a09e51129c4946f8704d4e52b89
Stefan Froemken <froemken@gmail.com>
2019-05-13 07:45:14 +0200

    Better description in ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,7 +12,9 @@
 
 $EM_CONF[$_EXTKEY] = [
     'title' => 'JW tools',
-    'description' => 'Tools for TYPO3 and various extensions',
+    'description' => 'Jwtools2 contains a scheduler task for Solr to index multiple Pagetrees and a task to execute 
+        SQL-Queries. Further there are settings to enable some features in TYPO3 like showing the Page UID in Pagetree
+        with a simple click in extensionmanager.',
     'category' => 'module',
     'author' => 'Stefan Froemken',
     'author_email' => 'projects@jweiland.net',
