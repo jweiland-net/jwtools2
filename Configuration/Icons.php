4a601dde63046d04a56fba5b1bb99dadd2381f09
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2024-01-29 11:36:43 +0100

    Icon registration moved to Configuration/Icons.php

---
+++ Configuration/Icons.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'ext-jwtools2-be-module-icon' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:jwtools2/Resources/Public/Icons/module_tools.svg'
+    ],
+];
