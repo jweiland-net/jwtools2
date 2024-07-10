84d496c1b0275aa3469f572e66f8777f749f37bb
Stefan Froemken <sfroemken@jweiland.net>
2024-07-10 13:35:27 +0200

    Allow throwing exception on invalid cache entries

--- ext_tables.sql
+++ ext_tables.sql
@@ -19,5 +19,7 @@ CREATE TABLE tx_jwtools2_stored_routes (
 CREATE TABLE tx_jwtools2_cache_expression (
 	title varchar(100) DEFAULT '' NOT NULL,
 	is_regexp tinyint(1) DEFAULT '0' NOT NULL,
+	is_exception tinyint(1) DEFAULT '0' NOT NULL,
+	exception_fe_only tinyint(1) DEFAULT '1' NOT NULL,
 	expression varchar(255) DEFAULT '' NOT NULL
 );
